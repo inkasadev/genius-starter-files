cb533f709066482b3a684a91f557661c591eda7e
Phillipe Martins <hi@inkasa.dev>
2021-03-17 00:19:12 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Genius
+
+Você já jogou Genius?
+
+Genius era um brinquedo muito popular na década de 1980. Distribuído pela Estrela, o brinquedo buscava estimular a memorização de cores e sons. Com um formato semelhante a um OVNI, possuía botões coloridos que emitiam sons harmônicos e se iluminavam em sequência. Cabia aos jogadores repetir o processo sem errar.
+
+Nesse repositório você encontra os *starter files* de um projeto de um jogo Genius completo, construído com HTML, CSS e Javascript.
+
+- Você pode conferir o resultado final do projeto [clicando aqui](https://genius.surge.sh/).
+- Assista ao passo a passo (dividido em 8 partes) clicando aqui.
+
+## O que você irá aprender ao terminar esse projeto:
+
+- CSS
+  - Variáveis CSS
+  - Unidade rem
+  - BEM
+  - Flexbox
+  - Pseudo-elementos
+  - Media Queries
+  
+- Javascript
+  - .addEventListener()
+  - .querySelector()
+  - .classList
+  - .forEach()
+  - .push()
+  - for Loop
+  - if else statements
+  - carregar sons
+  - setTimeout()
+  - setInterval()
+
---
+++ js/scripts.js
@@ -0,0 +1,88 @@
+const _data = {
+	gameOn: false,
+	timeout: undefined,
+	sounds: [],
+
+	strict: false,
+	playerCanPlay: false,
+	score: 0,
+	gameSequence: [],
+	playerSequence: []
+};
+
+const _gui = {
+	counter: document.querySelector(".gui__counter"),
+	switch: document.querySelector(".gui__btn-switch"),
+	led: document.querySelector(".gui__led"),
+	strict: document.querySelector(".gui__btn-strict"),
+	start: document.querySelector(".gui__btn-start"),
+	pads: document.querySelectorAll(".game__pad")
+}
+
+const _soundUrls = [
+	"audio/simonSound1.mp3",
+	"audio/simonSound2.mp3",
+	"audio/simonSound3.mp3",
+	"audio/simonSound4.mp3"
+];
+
+_soundUrls.forEach(sndPath => {
+	const audio = new Audio(sndPath);
+	_data.sounds.push(audio);
+});
+
+_gui.switch.addEventListener("click", () => {
+
+});
+
+_gui.strict.addEventListener("click", () => {
+
+});
+
+_gui.start.addEventListener("click", () => {
+
+});
+
+const padListener = (e) => {
+
+}
+
+_gui.pads.forEach(pad => {
+	pad.addEventListener("click", padListener);
+});
+
+const startGame = () => {
+
+}
+
+const setScore = () => {
+
+}
+
+const newColor = () => {
+
+}
+
+const playSequence = () => {
+
+}
+
+const blink = (text, callback) => {
+
+}
+
+const waitForPlayerClick = () => {
+
+}
+
+const resetOrPlayAgain = () => {
+
+}
+
+const changePadCursor = (cursorType) => {
+
+}
+
+const disablePads = () => {
+
+}
